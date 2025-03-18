e670b2f49ab52855ffa66ac6e0c7c5ab92eb3257
Adeepreddy <245122748038@mvsrec.edu.in>
2025-03-18 12:55:26 +0530

    Add files via upload

--- reversenumber.java
+++ reversenumber.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
+class reverse {
+    public static void main(String[] args) {
+        // Predefined input number
+        int number = 12345;  // You can change this value to test with different numbers
+        
+        // Variable to store the reversed number
+        int reversedNumber = 0;
+        
+        // Logic to reverse the number
+        while (number != 0) {
+            int digit = number % 10; // Get the last digit
+            reversedNumber = reversedNumber * 10 + digit; // Append digit to reversed number
+            number = number / 10; // Remove the last digit from the original number
+        }
+        
+        // Output the reversed number
+        System.out.println("Reversed number: " + reversedNumber);
+    }
+}
 
- class ReverseNumber {
-	 public static void main(String[] args) {
-		 Scanner scanner = new Scanner(System.in);
-			 System.out.print("Enter a number: ");
-		 int number = scanner.nextInt();
-		 int reversedNumber = 0;
-		 while (number != 0) {
-			 int digit = number % 10;
-			 reversedNumber = reversedNumber * 10 + digit; 
-			 number = number / 10;
-		 }
-		 System.out.println("Reversed number: " + reversedNumber);
-		 scanner.close();
-	 }
- }
